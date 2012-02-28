a48e7fa414828db175ac10ddfd73527fca849322
Xabier Rodriguez Calvar <xrcalvar@igalia.com>
2012-02-28 18:24:12 +0100

    Adding the effects manager to the qml context as effectManager

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <gst/gst.h>
 
 #include "controller.h"
-
+#include "effectmanager.h"
 #include "common.h"
 
 int main(int argc, char *argv[])
@@ -16,6 +16,9 @@ int main(int argc, char *argv[])
 
     QDeclarativeView view;
     qmlRegisterType<Controller>("aura.controller", 1, 0, "Controller");
+    QDeclarativeContext *context = view.rootContext();
+    EffectManager::setup();
+    context->setContextProperty("effectManager", EffectManager::instance());
     qDebug("opening qml files in %s", QML_PATH);
     view.setSource(QUrl::fromLocalFile(MAIN_QML_FILE));
     view.showFullScreen();
