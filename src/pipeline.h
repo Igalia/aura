9c8d688d9deedab9b0f4762f5476d869efa4cada
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2012-03-08 17:11:40 +0100

    add LGPL licencing

--- src/pipeline.h
+++ src/pipeline.h
@@ -1,3 +1,28 @@
+/*
+ * This file is part of aura
+ *
+ * Copyright (C) 2012 Igalia S.L.
+ *
+ * Contact: Miguel Gómez <magomez@igalia.com>
+ *          Xabier Rodriguez Calvar <xrcalvar@igalia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
 #ifndef PIPELINE_H
 #define PIPELINE_H
 
