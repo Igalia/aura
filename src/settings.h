0fc8633cfc42929d17d9372f79a20438f8a273f1
Víctor Manuel Jáquez Leal <vjaquez@igalia.com>
2012-03-19 18:14:23 +0100

    add me as an author too.

--- src/settings.h
+++ src/settings.h
@@ -5,6 +5,7 @@
  *
  * Contact: Miguel Gómez <magomez@igalia.com>
  *          Xabier Rodriguez Calvar <xrcalvar@igalia.com>
+ *          Víctor Jáquez <vjaquez@igalia.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
